477f6c0a6b6c6d35d90ff49f6c90cd0fccaacd51
Rupesh <rupesh.ch97@gmail.com>
2018-10-23 16:23:12 -0400

    Added a class to gather audio sample information and added transition probability calculations

--- HMM Idea/token_gen.py
+++ HMM Idea/token_gen.py
@@ -9,11 +9,17 @@ from sklearn.cluster import KMeans
 
 SAMPLE_RATE = 48000
 
+class AudioSample:
+	def __init__(self, filename, index, raw, features, cluster=None):
+		self.filename = filename
+		self.index = index
+		self.raw = raw
+		self.features = features
+		self.cluster = cluster
+
+
+# returns a list of (source file, index, sample, features) tuples
 def load_audio(data_path, label='*', sample_time=.25, pickle_read=True):
-	if pickle_read:
-		pickle_file = 'pickles/audio_{}.pkl'.format(label)
-		if os.path.exists(pickle_file):
-			return pickle.load(open(pickle_file, 'rb'))
 	audio_dir = '{}/{}/{}/*'.format(data_path, 'audio', label)
 	audio_samples = []
 	for audio_file in glob.glob(audio_dir):
@@ -22,31 +28,59 @@ def load_audio(data_path, label='*', sample_time=.25, pickle_read=True):
 		print(audio_file)
 		for index, offset in enumerate(np.arange(0, file_duration, sample_time)):
 			audio_raw, _ = librosa.core.load(audio_file, sr=SAMPLE_RATE, offset=offset, duration=sample_time)
-			sample_name = '{}_{}'.format(filename, index)
 			audio_chroma = librosa.feature.chroma_stft(audio_raw, sr=SAMPLE_RATE)
-			print(index, len(audio_chroma), sample_name)
-			audio_samples.append((sample_name, audio_raw, audio_chroma))
-	if pickle_read:
-		pickle_file = 'pickles/audio_{}.pkl'.format(label)
-		pickle.dump(audio_samples, open(pickle_file, 'wb+'))
+			features = np.ndarray.flatten(audio_chroma)
+			print(index, len(features), filename)
+			audio_samples.append(AudioSample(filename, index, audio_raw, features))
 	return audio_samples
 
+
 def cluster_audio(audio_samples, n_clusters):
-	audio_features = np.array([np.ndarray.flatten(np.array(features)) for _, _, features in audio_samples])
+	audio_features = np.array([sample.features for sample in audio_samples])
 	print(audio_features.shape)
 	kmeans = KMeans(n_clusters=n_clusters).fit(audio_features)
 	clusters = kmeans.labels_
 	print(clusters)
 	means = {i: a for i, a in enumerate(kmeans.cluster_centers_)}
-	pickle.dump(clusters, open('pickles/audio_clusters.pkl', 'wb+'))
-	pickle.dump(means, open('pickles/audio_cluster_centers.pkl', 'wb+'))
 	return clusters, means
 
-def main():
-	audio_samples = load_audio('..', label='ballet')
-	clusters, means = cluster_audio(audio_samples, 32)
+
+def main(pickle_data=True, label='*', n_clusters=32):
+	rewrite = True
+	pickle_samples = 'pickles/audio_{}_{}.pkl'.format(label, n_clusters)
+	pickle_means = 'pickles/means_{}_{}.pkl'.format(label, n_clusters)
+	audio_samples = means = None
+	if pickle_data:
+		if os.path.exists(pickle_samples) and os.path.exists(pickle_means):
+			with open(pickle_samples, 'rb') as f:
+				audio_samples = pickle.load(f)
+			with open(pickle_means, 'rb') as f:
+				means = pickle.load(f)
+			rewrite = False
+	if audio_samples is None or means is None:
+		audio_samples = load_audio('..', label=label)
+		clusters, means = cluster_audio(audio_samples, n_clusters)
+		for sample, cluster in zip(audio_samples, clusters):
+			sample.cluster = cluster
+	if pickle_data and rewrite:
+		with open(pickle_samples, 'wb+') as f:
+			pickle.dump(audio_samples, f)
+		with open(pickle_means, 'wb+') as f:
+			pickle.dump(means, f)
+	transitions = np.zeros((n_clusters, n_clusters))
+	totals = np.zeros(n_clusters)
+	for prev, curr in zip(audio_samples[:-1], audio_samples[1:]):
+		if prev.filename != curr.filename:
+			continue
+		transitions[prev.cluster][curr.cluster] += 1
+		totals[prev.cluster] += 1
+	print(transitions)
+	print(totals)
+	for i, (tr, to) in enumerate(zip(transitions, totals)):
+		print(tr[i], to, tr[i]/to)
+
 
 if __name__ == '__main__':
 	start_time = time.time()
-	main()
+	main(pickle_data=True, label='ballet', n_clusters=48)
 	print("This took {:4.2f}s".format(time.time()-start_time))
